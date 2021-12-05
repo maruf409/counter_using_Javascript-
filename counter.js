50ea64fd30a3947d9e57fe5e914fdd028917b7d2
maruf409 <67434334+maruf409@users.noreply.github.com>
2021-12-05 16:38:10 +0600

    source code

---
+++ counter.js
@@ -0,0 +1,11 @@
+var data = 0;
+//document.getElementById("counting").innerText = data;
+function increment(){
+    
+    data = data + 1;
+    document.getElementById("counting").innerText = data;
+}
+function decrement(){
+    
+    document.getElementById("counting").innerText = data;
+}
